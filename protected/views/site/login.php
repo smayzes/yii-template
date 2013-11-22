2c8b1a53cb7037b256d382adde9ee6f3f9e10328
Shawn Mayzes <shawn@codelabs.ca>
2013-11-21 17:09:59 -0800

    Updating view files.

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,47 +1,49 @@
 <?php
-/* @var $this SiteController */
-/* @var $model LoginForm */
-/* @var $form CActiveForm  */
-
 $this->pageTitle=Yii::app()->name . ' - Login';
+$this->breadcrumbs=array(
+	'Login',
+);
 ?>
-<section class="bck white padding text-shadow" id="about">
-	<div class="container">
-		<div class="hero-content">
-			<h1>Login</h1>
-			<?php 
-			if ( Yii::app()->user->hasFlash('register') ) {
-				echo TbHtml::alert(TbHtml::ALERT_COLOR_SUCCESS, Yii::app()->user->getFlash('register'));
-			}
-			?>
-			<?php $form=$this->beginWidget('CActiveForm', array(
-				'id'=>'login-form',
-				'enableClientValidation'=>true,
-				'clientOptions'=>array(
-					'validateOnSubmit'=>true,
-				),
-			)); ?>
-			
-				<p class="note">Fields with <span class="required">*</span> are required.</p>
-			
-				<div class="row">
-					<?php echo $form->labelEx($model,'username'); ?>
-					<?php echo $form->textField($model,'username'); ?>
-					<?php echo $form->error($model,'username'); ?>
-				</div>
-			
-				<div class="row">
-					<?php echo $form->labelEx($model,'password'); ?>
-					<?php echo $form->passwordField($model,'password'); ?>
-					<?php echo $form->error($model,'password'); ?>
-				</div>
-				
-				<div class="row buttons">
-					<?php echo CHtml::submitButton('Login'); ?>
-				</div>
-			
-			<?php $this->endWidget(); ?>
-			</div><!-- form -->
-		</div>
+
+<h1>Login</h1>
+
+<p>Please fill out the following form with your login credentials:</p>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'login-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username'); ?>
+		<?php echo $form->error($model,'username'); ?>
 	</div>
-</section>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password'); ?>
+		<?php echo $form->error($model,'password'); ?>
+		<p class="hint">
+			Hint: You may login with <tt>demo/demo</tt> or <tt>admin/admin</tt>.
+		</p>
+	</div>
+
+	<div class="row rememberMe">
+		<?php echo $form->checkBox($model,'rememberMe'); ?>
+		<?php echo $form->label($model,'rememberMe'); ?>
+		<?php echo $form->error($model,'rememberMe'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Login'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+</div><!-- form -->
